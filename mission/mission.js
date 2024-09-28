d42768ff3ada91cc722408d7ff11e92bac6d759b
gordonombaka <otienoombaka@gmail.com>
2024-09-28 17:17:19 +0300

    Completed the mission statement project

--- mission/mission.js
+++ mission/mission.js
@@ -1,7 +1,10 @@
-const themeSelector = document.querySelector("")// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+const themeSelector = document.querySelector("#theme-selector")// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
 function changeTheme() {
 // check to see what the current value of our select is.
 // The current value is conveniently found in themeSelector.value!
+
+const selectedTheme = themeSelector.value;
+const body = document.body;
 
 // if the value is dark then:
 // add the dark class to the body
@@ -9,6 +12,18 @@ function changeTheme() {
 // otherwise
 // remove the dark class
 // make sure the logo src is the blue logo.
+
+if (selectedTheme === "dark") {
+    body.classList.add("dark");
+    img.src = "byui-logo_blue.webp";
+    
+} 
+else {
+    body.classList.remove("dark");
+    img.src = "byui-logo_darktheme.png";
+
+}
+
 }
 
 // add an event listener to the themeSelector element here.
